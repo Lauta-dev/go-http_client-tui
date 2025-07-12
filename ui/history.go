63630ecb3e4957c8dd97d029d6531851269a31bb
lauta-dev <altairdiaz245@gmail.com>
2025-07-12 19:29:06 -0300

    añadido de colores y nuevos atajos

--- ui/history.go
+++ ui/history.go
@@ -15,6 +15,7 @@ DROP TABLE IF EXISTS request_history;
 
 import (
 	"fmt"
+	colors "http_client/const"
 	"http_client/logic"
 	"strconv"
 
@@ -22,21 +23,46 @@ import (
 	"github.com/rivo/tview"
 )
 
+func statusCodesColors(code int) string {
+	var color string
+	switch {
+
+	case code >= 100 && code < 200:
+		color = fmt.Sprintf("[%s::b]", colors.ColorTextPrimary.String())
+
+	case code >= 200 && code < 300:
+		color = fmt.Sprintf("[%s::b]", colors.ColorSuccess.String())
+
+	case code >= 300 && code < 400:
+		color = fmt.Sprintf("[%s::b]", colors.ColorWarning.String())
+
+	case code >= 400:
+		color = fmt.Sprintf("[%s::b]", colors.ColorError.String())
+
+	}
+
+	return fmt.Sprintf("%s[::B]", color)
+
+}
+
 func History(app *tview.Application) *tview.Flex {
 	list := tview.NewList()
 
 	list.SetBorder(true)
-	list.SetTitle(" > Historial de Request ('F2' para volver, 'F3' para actualizar lista)")
+	list.SetTitle(" > Historial de Request (F2: volver, u: actualizar lista)")
 	list.ShowSecondaryText(false)
+
+	logic.ApplySelectedBackgroundIfSupported(list, colors.ColorHighlight.TrueColor())
 
 	flex := tview.NewFlex()
 	responseView := ResponseView()
 
-	for _, v := range logic.Ggg() {
+	for _, v := range logic.GetAllItems() {
 		code := strconv.Itoa(v.StatusCode)
 
-		mainText := fmt.Sprintf("(%s, %s) - %s", v.Method, code, v.URL)
-		list.AddItem(mainText, v.ID, 'a', nil)
+		color := statusCodesColors(v.StatusCode)
+		mainText := fmt.Sprintf("%s %s, %s [white]- %s", color, v.Method, code, v.URL)
+		list.AddItem(mainText, v.ID, 0, nil)
 	}
 
 	list.SetChangedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
@@ -49,7 +75,14 @@ func History(app *tview.Application) *tview.Flex {
 			return
 		}
 
-		fmt.Fprintf(responseView, "%s", row.ResponseBody)
+		fmt.Fprintf(responseView,
+			"URL: %s\nMethod: %s\nCode: %s\nContent Type: %s\n\n%s",
+			row.URL,
+			row.Method,
+			strconv.Itoa(row.StatusCode),
+			row.ContentType,
+			row.ResponseBody,
+		)
 
 	})
 
@@ -57,26 +90,34 @@ func History(app *tview.Application) *tview.Flex {
 	flex.AddItem(responseView, 0, 1, false)
 
 	list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		switch event.Key() {
-		case tcell.KeyF3:
-			list.Clear()
-			for _, v := range logic.Ggg() {
-				code := strconv.Itoa(v.StatusCode)
-
-				mainText := fmt.Sprintf("(%s, %s) - %s", v.Method, code, v.URL)
-				list.AddItem(mainText, v.ID, 'a', nil)
-			}
-
-		}
-
-		return event
-	})
-
-	list.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-
 		switch event.Rune() {
 		case 'k':
 			app.SetFocus(responseView)
+
+		case 'r':
+			i := list.GetCurrentItem()
+			_, id := list.GetItemText(i)
+
+			_, err := logic.DelItems(id)
+
+			if err != nil {
+				responseView.Clear()
+				fmt.Fprintf(responseView, "[red]%s", err.Error())
+				return nil
+			}
+
+			list.RemoveItem(i)
+			return nil
+
+		case 'u':
+			list.Clear()
+			for _, v := range logic.GetAllItems() {
+				code := strconv.Itoa(v.StatusCode)
+
+				color := statusCodesColors(v.StatusCode)
+				mainText := fmt.Sprintf("%s %s, %s [white]- %s", color, v.Method, code, v.URL)
+				list.AddItem(mainText, v.ID, 0, nil)
+			}
 		}
 
 		return event
