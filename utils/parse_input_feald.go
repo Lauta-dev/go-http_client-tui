d24bdaaa89d0a5fc7d52503117530f4ada9f5cf3
lauta-dev <altairdiaz245@gmail.com>
2025-07-12 21:07:18 -0300

    Ahora lanza un error si no se encuentra alguna variable

--- utils/parse_input_feald.go
+++ utils/parse_input_feald.go
@@ -1,24 +1,32 @@
 package utils
 
 import (
+	"fmt"
 	"strings"
 )
 
-func ParseInput(input string, variables map[string]string) string {
+func ParseInput(input string, variables map[string]string) (string, error) {
 	/*re := regexp.MustCompile(`@[^/\s]+(?=[/ ]|$)`)
 	matches := re.FindAllString(input, -1)*/
 
 	s := strings.Split(input, "/")
+	noMatch := ""
 
 	for i, v := range s {
-
-		for k, variable := range variables {
-			if v == "@"+k {
-				s[i] = variable
+		if strings.HasPrefix(v, "@") {
+			key := v[1:] // Quitar la @
+			val, ok := variables[key]
+			if ok {
+				s[i] = val
+			} else {
+				noMatch += v + "\n" // Solo agrega el no encontrado
 			}
 		}
-
 	}
 
-	return strings.Join(s, "/")
+	if noMatch != "" {
+		return "", fmt.Errorf("Algunos elemenos no se encuentar en la pestaña de variables: \n%s", noMatch)
+	}
+
+	return strings.Join(s, "/"), nil
 }
