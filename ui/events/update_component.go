ff53fce681ba719b1ecb2172d2a0cca680330a08
lauta-dev <altairdiaz245@gmail.com>
2025-08-15 17:27:25 -0300

    ahora las cosas estan mas separadas

--- ui/events/update_component.go
+++ ui/events/update_component.go
@@ -18,6 +18,58 @@ type UIController struct {
 
 }
 
+func (ui *UIController) clearResponses() {
+	ui.ResponseView.Clear()
+	ui.ResponseInfo.Clear()
+}
+
+func (ui *UIController) showLoading() {
+	ui.App.QueueUpdateDraw(func() {
+		ui.clearResponses()
+
+		loadingFormat := fmt.Sprintf("[%s::b]%s[%s::B]",
+			color.ColorTextSecondary.String(),
+			"Cargando...",
+			color.ColorPrimary.String(),
+		)
+
+		fmt.Fprintln(ui.ResponseView, loadingFormat)
+		fmt.Fprintln(ui.ResponseInfo, loadingFormat)
+	})
+}
+
+func (ui *UIController) showError(err error) {
+	ui.App.QueueUpdateDraw(func() {
+		ui.clearResponses()
+		fmt.Fprintln(ui.ResponseView, err.Error())
+	})
+}
+
+func (ui *UIController) displayResponse(res logic.Fetch, saveRequest bool, method string) {
+	ui.App.QueueUpdateDraw(func() {
+		ui.clearResponses()
+
+		format := utils.ResponseInfoFormat(res.ContentType, res.UserUrl, res.Status)
+		code := strings.Split(res.Status, " ")[0]
+
+		if saveRequest {
+			if err := logic.SaveItems(res.UserUrl, code, res.ContentType, res.Body, method); err != nil {
+				fmt.Fprintf(ui.ResponseInfo, "[red]%s", err.Error())
+			}
+		}
+
+		fmt.Fprintln(ui.ResponseInfo, format)
+
+		display, err := utils.PrettyStyle(res.ContentType, []byte(res.Body), ui.ResponseView)
+		ui.ContentToCopy = &display
+
+		if err != nil {
+			ui.ResponseView.Clear()
+			fmt.Fprintf(ui.ResponseView, "Error al colorear la salida\n%s\n\n%s", err.Error(), display)
+		}
+	})
+}
+
 func (ui *UIController) UpdateComponent(
 	httpMethod string,
 	headers map[string]string,
@@ -26,59 +78,13 @@ func (ui *UIController) UpdateComponent(
 	body string,
 	userUrl string, saveRequest bool) {
 
-	app := ui.App
-	responseView := ui.ResponseView
-	responseInfo := ui.ResponseInfo
-
-	app.QueueUpdateDraw(func() {
-		responseView.Clear()
-		responseInfo.Clear()
-
-		loadingFormat := fmt.Sprintf("[%s::b]%s[%s::B]",
-			color.ColorTextSecondary.String(),
-			"Cargando...",
-			color.ColorPrimary.String(),
-		)
-
-		fmt.Fprintln(responseView, loadingFormat)
-		fmt.Fprintln(responseInfo, loadingFormat)
-	})
+	ui.showLoading()
 
 	res, err := logic.Fetching(userUrl, httpMethod, headers, queryParams, params, body)
 
 	if err != nil {
-		app.QueueUpdateDraw(func() {
-			responseInfo.Clear()
-			responseView.Clear()
-
-			fmt.Fprintln(responseView, err.Error())
-		})
-
-		return
+		ui.showError(err)
 	}
 
-	app.QueueUpdateDraw(func() {
-		responseInfo.Clear()
-		responseView.Clear()
-
-		format := utils.ResponseInfoFormat(res.ContentType, res.UserUrl, res.Status)
-		code := strings.Split(res.Status, " ")[0]
-		if saveRequest {
-			err := logic.SaveItems(res.UserUrl, code, res.ContentType, res.Body, httpMethod)
-
-			if err != nil {
-				fmt.Fprintf(responseInfo, "[red]%s", err.Error())
-			}
-		}
-		fmt.Fprintln(responseInfo, format)
-
-		display, err := utils.PrettyStyle(res.ContentType, []byte(res.Body), responseView)
-		ui.ContentToCopy = &display
-
-		if err != nil {
-			responseView.Clear()
-			fmt.Fprintf(responseView, "Error al colorar la salída\n%s\n\n%s", err.Error(), display)
-		}
-
-	})
+	ui.displayResponse(res, saveRequest, httpMethod)
 }
